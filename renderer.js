3e6f0a18376c1708b8493b89b137b4b6b41071d0
ADaunys <augustas.daunys@ktu.edu>
2022-05-11 20:18:06 +0300

    Not important

--- renderer.js
+++ renderer.js
@@ -4,74 +4,86 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
-	import QrScanner from "./qr-scanner.min.js";
-	
-	const video = document.getElementById('qr-video');
-    const videoContainer = document.getElementById('video-container');
-    const camHasCamera = document.getElementById('cam-has-camera');
-    const camList = document.getElementById('cam-list');
-    const camHasFlash = document.getElementById('cam-has-flash');
-    const flashToggle = document.getElementById('flash-toggle');
-    const flashState = document.getElementById('flash-state');
-    const camQrResult = document.getElementById('cam-qr-result');
-    const camQrLastResult = document.getElementById('cam-qr-last-result');
+import QrScanner from "./qr-scanner.min.js";
 
-    function setResult(label, result) {
-        console.log(result.data);
-        label.textContent = result.data;
-        camQrLastResult.textContent = result.data;
-        label.style.color = 'teal';
-        clearTimeout(label.highlightTimeout);
-        label.highlightTimeout = setTimeout(() => label.style.color = 'inherit', 100);
-    }
+const video = document.getElementById("qr-video");
+const videoContainer = document.getElementById("video-container");
+const camHasCamera = document.getElementById("cam-has-camera");
+const camList = document.getElementById("cam-list");
+const camHasFlash = document.getElementById("cam-has-flash");
+const flashToggle = document.getElementById("flash-toggle");
+const flashState = document.getElementById("flash-state");
+const camQrResult = document.getElementById("cam-qr-result");
+const camQrLastResult = document.getElementById("cam-qr-last-result");
 
-    // ####### Web Cam Scanning #######
+function setResult(label, result) {
+  console.log(result.data);
+  label.textContent = result.data;
+  camQrLastResult.textContent = result.data;
+  label.style.color = "teal";
+  clearTimeout(label.highlightTimeout);
+  label.highlightTimeout = setTimeout(
+    () => (label.style.color = "inherit"),
+    100
+  );
+}
 
-    const scanner = new QrScanner(video, result => setResult(camQrResult, result), {
-        onDecodeError: error => {
-            camQrResult.textContent = error;
-            camQrResult.style.color = 'inherit';
-        },
-        highlightScanRegion: true,
-        highlightCodeOutline: true,
-    });
+// ####### Web Cam Scanning #######
 
-    const updateFlashAvailability = () => {
-        scanner.hasFlash().then(hasFlash => {
-            camHasFlash.textContent = hasFlash;
-            flashToggle.style.display = hasFlash ? 'inline-block' : 'none';
-        });
-    };
+const scanner = new QrScanner(
+  video,
+  (result) => setResult(camQrResult, result),
+  {
+    onDecodeError: (error) => {
+      camQrResult.textContent = error;
+      camQrResult.style.color = "inherit";
+    },
+    highlightScanRegion: true,
+    highlightCodeOutline: true,
+  }
+);
 
-    scanner.start().then(() => {
-		
-        updateFlashAvailability();
-        // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
-        // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
-        // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
-        // start the scanner earlier.
-        QrScanner.listCameras(true).then(cameras => cameras.forEach(camera => {
-            const option = document.createElement('option');
-            option.value = camera.id;
-            option.text = camera.label;
-            camList.add(option);
-        }));
-    });
+const updateFlashAvailability = () => {
+  scanner.hasFlash().then((hasFlash) => {
+    camHasFlash.textContent = hasFlash;
+    flashToggle.style.display = hasFlash ? "inline-block" : "none";
+  });
+};
 
-    QrScanner.hasCamera().then(hasCamera => camHasCamera.textContent = hasCamera);
+scanner.start().then(() => {
+  updateFlashAvailability();
+  // List cameras after the scanner started to avoid listCamera's stream and the scanner's stream being requested
+  // at the same time which can result in listCamera's unconstrained stream also being offered to the scanner.
+  // Note that we can also start the scanner after listCameras, we just have it this way around in the demo to
+  // start the scanner earlier.
+  QrScanner.listCameras(true).then((cameras) =>
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.id;
+      option.text = camera.label;
+      camList.add(option);
+    })
+  );
+});
 
-    camList.addEventListener('change', event => {
-        scanner.setCamera(event.target.value).then(updateFlashAvailability);
-    });
+QrScanner.hasCamera().then(
+  (hasCamera) => (camHasCamera.textContent = hasCamera)
+);
 
-    flashToggle.addEventListener('click', () => {
-        scanner.toggleFlash().then(() => flashState.textContent = scanner.isFlashOn() ? 'on' : 'off');
-    });
+camList.addEventListener("change", (event) => {
+  scanner.setCamera(event.target.value).then(updateFlashAvailability);
+});
 
-    document.getElementById('start-button').addEventListener('click', () => {
-        scanner.start();
-    });
+flashToggle.addEventListener("click", () => {
+  scanner
+    .toggleFlash()
+    .then(() => (flashState.textContent = scanner.isFlashOn() ? "on" : "off"));
+});
 
-    document.getElementById('stop-button').addEventListener('click', () => {
-        scanner.stop();
-    });
+document.getElementById("start-button").addEventListener("click", () => {
+  scanner.start();
+});
+
+document.getElementById("stop-button").addEventListener("click", () => {
+  scanner.stop();
+});
